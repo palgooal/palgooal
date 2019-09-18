29ffa80170d5db0ec431034e1a06f77025d08025
Pal Goal <palgooal@gmail.com>
2019-09-18 14:42:25 +0300

    set lang

--- gooalco/app/Http/Controllers/HomeController.php
+++ gooalco/app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Session;
 
 class HomeController extends Controller
 {
@@ -11,10 +12,10 @@ class HomeController extends Controller
      *
      * @return void
      */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+    // public function __construct()
+    // {
+    //     $this->middleware('auth');
+    // }
 
     /**
      * Show the application dashboard.
@@ -24,5 +25,9 @@ class HomeController extends Controller
     public function index()
     {
         return view('home');
+    }
+    function lang($local){
+        Session::put('lang', $local);
+        return redirect()->back();
     }
 }
