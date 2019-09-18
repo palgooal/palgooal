47b1cfbc2640281d96a8a81b82593f65a4ad6916
islam shublaq <islamshu12@gmail.com>
2019-09-18 13:56:44 +0300

    add Post controller

---
+++ gooalco/database/factories/PostFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\modals\Post;
+use Faker\Generator as Faker;
+
+$factory->define(Post::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
--- gooalco/routes/web.php
+++ gooalco/routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
+Route::get('/', function () {
+    return view('index');
+});
 Route::get('/admin', function () {
     return view('admin.welcom');
 });
