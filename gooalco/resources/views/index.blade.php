75d6141676cbda11516cbbb17ffeb64baa57653e
abedalaa yaghi <abedalaayaghi@gmail.com>
2019-09-27 20:22:42 +0300

    serves

--- gooalco/resources/views/index.blade.php
+++ gooalco/resources/views/index.blade.php
@@ -130,20 +130,52 @@ Find your Domain: <input type="text" name="query" size="20" />
 
 </div>
 
-<div class="container-fluid ptb80 info" id="secondary">
-<div class="container">
+</section>
+<section class="servic-gooel">
+<div class="feat bg-gray pt-5 pb-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="section-head col-sm-12">
+        <br />
+          <h4><span>Why Choose</span> Me?</h4>
+          <br />
+          <p>Lorem Ipsum is simply dummy text of the printing and type setting industry. Lorem Ipsum has been the industry's<br>standard dummy text ever since the 1500s, when an unknown book.</p>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_one"><i class="fa fa-globe"></i></span>
+            <h6>Modern Design</h6>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_two"><i class="fa fa-anchor"></i></span>
+            <h6>Creative Design</h6>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_three"><i class="fa fa-anchor"></i></span>
+            <h6>Your Photoshoping</h6>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_four"><i class="fa fa-database"></i></span>
+            <h6>Business Growth</h6>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_five"><i class="fa fa-upload"></i></span>
+            <h6>Market Strategy</h6>
+          </div>
+        </div>
+        <div class="col-lg-4 col-sm-6">
+          <div class="item"> <span class="icon feature_box_col_six"><i class="fa fa-camera"></i></span>
+            <h6>Retina Ready</h6>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
-<div class="col-md-offset-2 col-md-6 info-text">
-<h3 class="pb20">Our main service</h3>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-<ul>
-<li>Unique & modern design.</li>
- <li>Svg graphics.</li>
-<li>Retina ready.</li>
-</ul>
-</div>
-</div>
-</div>
+
 </section>
 <section class="portfolio pt80 pb40">
         <div class="container nopadding">
